266e3fb4986fcb09e64815c7239ffe0852d40c80
Amrysliu <xiaojing.liu@suse.com>
2021-08-17 15:41:36 +0800

    this is for test

---
+++ tests/security/cc/test_demo.pm
@@ -0,0 +1,32 @@
+# SUSE's openQA tests
+#
+# Copyright © 2021 SUSE LLC
+#
+# Copying and distribution of this file, with or without modification,
+# are permitted in any medium without royalty provided the copyright
+# notice and this notice are preserved.  This file is offered as-is,
+# without any warranty.
+#
+# Summary: Run 'filter' test case of 'audit-test' test suite
+# Maintainer: Liu Xiaojing <xiaojing.liu@suse.com>
+# Tags: poo#95464
+
+use base 'consoletest';
+use strict;
+use warnings;
+use testapi;
+use utils;
+use audit_test qw(run_testcase compare_run_log);
+
+sub run {
+    my ($self) = shift;
+
+    select_console 'root-console';
+
+    script_run('unamr -r');
+
+    my $a = "1";
+    my $b = "2";
+}
+
+1;
