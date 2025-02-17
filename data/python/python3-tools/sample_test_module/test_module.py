cb41e18cd241d0509e603cf6bced605d5e4913ce
Xiaojing_liu <xiaojing.liu@suse.com>
2025-02-17 16:23:59 +0800

    test

---
+++ data/python/python3-tools/sample_test_module/test_module.py
@@ -0,0 +1,5 @@
+# test_module.py
+from user_package_tools import my_module
+
+result = my_module.add(2, 3)
+assert result == 5 
---
+++ data/python/python3-tools/setup.py
@@ -0,0 +1,6 @@
+from tools import setup
+setup(
+    name='user_package_tools',  # Ensure this matches the package directory name
+    version='1.0',
+    packages=['user_package_tools'],
+)
